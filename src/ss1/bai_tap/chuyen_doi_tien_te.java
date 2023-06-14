076f56082524d05640241dcbc33e849955aa3087
HieuTranROM <trunghieu17082001@gmail.com>
2023-06-14 21:02:24 +0700

    nop bai tap

--- src/ss1/bai_tap/chuyen_doi_tien_te.java
+++ src/ss1/bai_tap/chuyen_doi_tien_te.java
@@ -1,2 +1,15 @@
-package ss1.bai_tap;public class chuyen_doi_tien_te {
-}
+package ss1.bai_tap;
+
+import java.util.Scanner;
+public class chuyen_doi_tien_te {
+        public static void main(String[] args) {
+            double vnd = 23000;
+            double usd;
+            Scanner sc = new Scanner(System.in);
+            System.out.print(" Nhap so tien can chuyen doi: ");
+            usd = sc.nextDouble();
+            double quydoi = usd * 23000;
+            System.out.println(" Gia tri vnd: " + quydoi);
+        }
+    }
+
