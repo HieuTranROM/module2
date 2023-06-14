13ff4aee8e1c92b887c1a4739d57757a517686ac
HieuTranROM <trunghieu17082001@gmail.com>
2023-06-14 19:24:00 +0700

    nop bai tap

---
+++ src/ss1/bai_tap/Name.java
@@ -0,0 +1,13 @@
+package ss1.bai_tap;
+
+import java.util.Scanner;
+
+public class Name {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello: " + name);
+
+    }
+}
